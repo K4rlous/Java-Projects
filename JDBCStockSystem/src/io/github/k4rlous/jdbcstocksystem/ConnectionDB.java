fcd5915b0d1817425e99f49d09fea16580d41616
K4rlous <carlloshenrique.1971@gmail.com>
2025-11-19 00:59:10 -0300

    feat: initial JDBC project setup
    - Added lib folder with dependencies (SQLite + SLF4J)
    - Implemented ConnectionDB, TableCreator, and Products classes
    - Prepared base structure for future stock management features

---
+++ JDBCStockSystem/src/io/github/k4rlous/jdbcstocksystem/ConnectionDB.java
@@ -0,0 +1,31 @@
+package io.github.k4rlous.jdbcstocksystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    // Se o arquivo não existir, isso irá criá-lo!
+    private static final String URL_JDBC_PADRAO = "jdbc:sqlite:meu_banco_de_dados.db";
+
+    // Método conectar padrão
+    public static Connection conectar(){
+        try{
+            return DriverManager.getConnection(URL_JDBC_PADRAO);
+        } catch(SQLException e){
+            // o 'err' é similar ao 'out', porém dá destaque em vermelho a mensagem em servidores e terminais aptos, dando mais clareza!
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            return null;
+        }
+    }
+
+    // Método para conectar com url, usuário e senha
+    public static Connection conectarGenerico(String url, String usuario, String senha){
+        try{
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch(SQLException e){
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ JDBCStockSystem/src/io/github/k4rlous/jdbcstocksystem/Products.java
@@ -0,0 +1,61 @@
+package io.github.k4rlous.jdbcstocksystem;
+
+public class Products {
+    private int id;
+    private String nome;
+    private int quantidade;
+    private double preco;
+    private String status;
+
+    // O ID é autoincrementado pelo próprio banco de dados!
+    public Products(String nome, int quantidade, double preco, String status) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.status = status;
+    }
+
+    public Products(){
+        // Construtor vazio
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
